a4e5355080ad5ce7d537b0dae0bfee9b0fd0a06f
Luan Kosaka <luan.kosaka@webjump.com.br>
2019-11-14 00:53:59 -0300

    mutation

--- mutation/projeto/resolvers/index.js
+++ mutation/projeto/resolvers/index.js
@@ -1,7 +1,8 @@
 const Query = require('./Query')
 const Produto = require('./Produto')
 const Usuario = require('./Usuario')
+const Mutation = require('./Mutation')
 
 module.exports = {
-    Query, Produto, Usuario
+    Query, Produto, Usuario, Mutation
 }
